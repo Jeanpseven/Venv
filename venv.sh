b669c197d4267d8eed426c3c3069a17d676b7355
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-20 14:50:19 -0300

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -3,17 +3,20 @@
 # Nome fixo para o ambiente virtual
 venv_name="venv"
 
+# Obtém o caminho para o diretório do script
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Caminho para o diretório onde o ambiente virtual será criado
-venv_path=$(realpath "$venv_name")
+venv_path="$script_dir/$venv_name"
 
 # Comando para criar o ambiente virtual
 python3 -m venv "$venv_path"
 
-# Comando para ativar o ambiente virtual
-activate_venv="$venv_path/bin/activate"
-
-# Ativa automaticamente o ambiente virtual
-source "$activate_venv"
-
 # Exibe instruções
-echo "O ambiente virtual foi criado e ativado em $venv_path."
+echo "O ambiente virtual foi criado em $venv_path."
+
+# Executa o comando para ativar o ambiente virtual
+source "$venv_path/bin/activate"
+
+# Exibe mensagem adicional
+echo "Ambiente virtual ativado. Execute 'deactivate' para sair do ambiente."
